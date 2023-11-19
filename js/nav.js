9cfda0972c791b48dd1f99981d333b8e3cc843fd
rrs12 <rahilsalecha12@gmail.com>
2023-11-19 23:05:59 +0530

    update

---
+++ js/nav.js
@@ -0,0 +1,22 @@
+const navLinks = document.querySelector('.nav-links')
+const logo_text = document.querySelector('.logo_text')
+const menu_button = document.querySelector('.menu_button')
+const main_navbar = document.querySelector('#unhide')
+
+function onToggleMenu(e) {
+    e.name = e.name === 'menu' ? 'close' : 'menu'
+    navLinks.classList.toggle('left-[3%]')
+    navLinks.classList.toggle('shadow')
+    navLinks.classList.toggle('bg-[#0b1446]')
+    main_navbar.classList.toggle('hidden')
+}
+function hide() {
+    menu_button.name = menu_button === 'menu' ? 'close' : 'menu'
+    navLinks.classList.toggle('left-[3%]')
+    navLinks.classList.toggle('md:left-[1%]')
+    navLinks.classList.toggle('shadow')
+    navLinks.classList.toggle('lg:shadow-none')
+    navLinks.classList.toggle('bg-[#0b1446]')
+    navLinks.classList.toggle('lg:bg-transparent')
+    main_navbar.classList.toggle('hidden')
+}
