dfd3f419bc4807e12becf5718beedbda05532bd4
rrs12 <rahilsalecha12@gmail.com>
2023-11-26 19:39:57 +0530

    update

--- js/nav.js
+++ js/nav.js
@@ -2,13 +2,19 @@ const navLinks = document.querySelector('.nav-links')
 const logo_text = document.querySelector('.logo_text')
 const menu_button = document.querySelector('.menu_button')
 const main_navbar = document.querySelector('#unhide')
+const body= document.querySelector("body")
+const hero=document.querySelector(".hero_sec")
 
 function onToggleMenu(e) {
     e.name = e.name === 'menu' ? 'close' : 'menu'
     navLinks.classList.toggle('left-[3%]')
     navLinks.classList.toggle('shadow')
-    navLinks.classList.toggle('bg-[#0b1446]')
+    navLinks.classList.toggle('bg-[#0b1446e2]')
     main_navbar.classList.toggle('hidden')
+    $("html").animate({ scrollTop: 0 }, "slow");
+    body.classList.toggle("overflow-hidden")
+    logo_text.classList.toggle("blur-sm")
+    hero.classList.toggle("blur-md")
 
 }
 function hide() {
@@ -17,7 +23,10 @@ function hide() {
     navLinks.classList.toggle('md:left-[1%]')
     navLinks.classList.toggle('shadow')
     navLinks.classList.toggle('lg:shadow-none')
-    navLinks.classList.toggle('bg-[#0b1446]')
+    navLinks.classList.toggle('bg-[#0b1446e2]')
     navLinks.classList.toggle('lg:bg-transparent')
     main_navbar.classList.toggle('hidden')
+    body.classList.toggle("overflow-hidden")
+    hero.classList.toggle("blur-md")
+
 }
