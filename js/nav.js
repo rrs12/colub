0ce4cbc88d9580f2965d2b4d95833bf0b290d433
rrs12 <rahilsalecha12@gmail.com>
2023-11-26 18:24:03 +0530

    update

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,6 @@ const navLinks = document.querySelector('.nav-links')
 const logo_text = document.querySelector('.logo_text')
 const menu_button = document.querySelector('.menu_button')
 const main_navbar = document.querySelector('#unhide')
-const hero= document.querySelector(".hero_sec")
-const banner_img= document.querySelector(".banner_image")
 
 function onToggleMenu(e) {
     e.name = e.name === 'menu' ? 'close' : 'menu'
@@ -11,8 +9,6 @@ function onToggleMenu(e) {
     navLinks.classList.toggle('shadow')
     navLinks.classList.toggle('bg-[#0b1446]')
     main_navbar.classList.toggle('hidden')
-    hero.classList.toggle("blur-sm")
-    logo_text.classList.toggle("blur-sm")
 
 }
 function hide() {
