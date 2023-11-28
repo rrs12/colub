ec98e32487e0aaf6648fdbcdca2a36d42f0f7101
rrs12 <rahilsalecha12@gmail.com>
2023-11-28 22:19:44 +0530

    update

--- js/owl.js
+++ js/owl.js
@@ -12,5 +12,28 @@ $(document).ready(function() {
         items : 1, 
         center: true,
        });
+
+       $("#owl-demo_2").owlCarousel({
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        917:{
+            items:3,
+        },
+    
+    }
+       });
+   
    
   });
+
+
